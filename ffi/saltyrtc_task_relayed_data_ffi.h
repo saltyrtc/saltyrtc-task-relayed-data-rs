ac0344c225013d3714893ebb1fe2aea31e995c95
Danilo Bargen <danilo.bargen@threema.ch>
2019-10-14 17:40:20 +0200

    Upgrade cbindgen

--- ffi/saltyrtc_task_relayed_data_ffi.h
+++ ffi/saltyrtc_task_relayed_data_ffi.h
@@ -6,9 +6,10 @@
 #ifndef saltyrtc_task_relayed_data_bindings_h
 #define saltyrtc_task_relayed_data_bindings_h
 
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define LEVEL_DEBUG 1
 
@@ -22,315 +23,315 @@
 
 #define LEVEL_WARN 3
 
-/*
+/**
  * Result type with all potential connection error codes.
  *
  * If no error happened, the value should be `CONNECT_OK` (0).
  */
 enum salty_client_connect_success_t {
-  /*
+  /**
    * No error.
    */
   CONNECT_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   CONNECT_NULL_ARGUMENT = 1,
-  /*
+  /**
    * The hostname is invalid (probably not UTF-8)
    */
   CONNECT_INVALID_HOST = 2,
-  /*
+  /**
    * TLS related error
    */
   CONNECT_TLS_ERROR = 3,
-  /*
+  /**
    * Certificate related error
    */
   CONNECT_CERTIFICATE_ERROR = 4,
-  /*
+  /**
    * Another connection error
    */
   CONNECT_ERROR = 9,
 };
 typedef uint8_t salty_client_connect_success_t;
 
-/*
+/**
  * Result type with all potential disconnection error codes.
  *
  * If no error happened, the value should be `DISCONNECT_OK` (0).
  */
 enum salty_client_disconnect_success_t {
-  /*
+  /**
    * No error.
    */
   DISCONNECT_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   DISCONNECT_NULL_ARGUMENT = 1,
-  /*
+  /**
    * Another connection error
    */
   DISCONNECT_ERROR = 9,
 };
 typedef uint8_t salty_client_disconnect_success_t;
 
-/*
+/**
  * Result type with all potential encrypt/decrypt error codes.
  *
  * If no error happened, the value should be `ENCRYPT_DECRYPT_OK` (0).
  */
 enum salty_client_encrypt_decrypt_success_t {
-  /*
+  /**
    * No error.
    */
   ENCRYPT_DECRYPT_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   ENCRYPT_DECRYPT_NULL_ARGUMENT = 1,
-  /*
+  /**
    * The peer has not yet been determined.
    */
   ENCRYPT_DECRYPT_NO_PEER = 2,
-  /*
+  /**
    * Other error
    */
   ENCRYPT_DECRYPT_ERROR = 9,
 };
 typedef uint8_t salty_client_encrypt_decrypt_success_t;
 
-/*
+/**
  * Result type with all potential init error codes.
  *
  * If no error happened, the value should be `INIT_OK` (0).
  */
 enum salty_client_init_success_t {
-  /*
+  /**
    * No error.
    */
   INIT_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   INIT_NULL_ARGUMENT = 1,
-  /*
+  /**
    * The hostname is invalid (probably not UTF-8)
    */
   INIT_INVALID_HOST = 2,
-  /*
+  /**
    * TLS related error
    */
   INIT_TLS_ERROR = 3,
-  /*
+  /**
    * Certificate related error
    */
   INIT_CERTIFICATE_ERROR = 4,
-  /*
+  /**
    * Another initialization error
    */
   INIT_ERROR = 9,
 };
 typedef uint8_t salty_client_init_success_t;
 
-/*
+/**
  * Result type with all potential event receiving error codes.
  *
  * If no error happened, the value should be `RECV_OK` (0).
  */
 enum salty_client_recv_success_t {
-  /*
+  /**
    * No error.
    */
   RECV_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   RECV_NULL_ARGUMENT = 1,
-  /*
+  /**
    * No data is available (timeout reached).
    */
   RECV_NO_DATA = 2,
-  /*
+  /**
    * The stream has ended and *SHOULD NOT* be polled again.
    */
   RECV_STREAM_ENDED = 3,
-  /*
+  /**
    * Another receiving error
    */
   RECV_ERROR = 9,
 };
 typedef uint8_t salty_client_recv_success_t;
 
-/*
+/**
  * Result type with all potential error codes.
  *
  * If no error happened, the value should be `SEND_OK` (0).
  */
 enum salty_client_send_success_t {
-  /*
+  /**
    * No error.
    */
   SEND_OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   SEND_NULL_ARGUMENT = 1,
-  /*
+  /**
    * Sending failed because the message was invalid
    */
   SEND_MESSAGE_ERROR = 2,
-  /*
+  /**
    * Sending failed
    */
   SEND_ERROR = 9,
 };
 typedef uint8_t salty_client_send_success_t;
 
-/*
+/**
  * Possible event types.
  */
 enum salty_event_type_t {
-  /*
+  /**
    * A connection is being established.
    */
   EVENT_CONNECTING = 1,
-  /*
+  /**
    * Server handshake completed.
    */
   EVENT_SERVER_HANDSHAKE_COMPLETED = 2,
-  /*
+  /**
    * Peer handshake completed.
    */
   EVENT_PEER_HANDSHAKE_COMPLETED = 3,
-  /*
+  /**
    * A peer has disconnected from the server.
    */
   EVENT_PEER_DISCONNECTED = 4,
 };
 typedef uint8_t salty_event_type_t;
 
-/*
+/**
  * Possible message types.
  */
 enum salty_msg_type_t {
-  /*
+  /**
    * Incoming task message
    */
   MSG_TASK = 1,
-  /*
+  /**
    * Incoming application message.
    */
   MSG_APPLICATION = 2,
-  /*
+  /**
    * Incoming close message.
    */
   MSG_CLOSE = 3,
 };
 typedef uint8_t salty_msg_type_t;
 
-/*
+/**
  * Result type with all potential error codes.
  *
  * If no error happened, the value should be `OK` (0).
  */
 enum salty_relayed_data_success_t {
-  /*
+  /**
    * No error.
    */
   OK = 0,
-  /*
+  /**
    * One of the arguments was a `null` pointer.
    */
   NULL_ARGUMENT = 1,
-  /*
+  /**
    * Creation of the object failed.
    */
   CREATE_FAILED = 2,
-  /*
+  /**
    * The public key bytes are not valid.
    */
   PUBKEY_INVALID = 3,
-  /*
+  /**
    * The auth token bytes are not valid.
    */
   AUTH_TOKEN_INVALID = 4,
-  /*
+  /**
    * The trusted key bytes are not valid.
    */
   TRUSTED_KEY_INVALID = 5,
-  /*
+  /**
    * The server permanent public key bytes are not valid.
    */
   SERVER_KEY_INVALID = 6,
 };
 typedef uint8_t salty_relayed_data_success_t;
 
-/*
+/**
  * The oneshot channel for closing the connection (receiving end).
  *
  * On the Rust side, this is an `oneshot::Receiver<CloseCode>`.
  */
 typedef struct salty_channel_disconnect_rx_t salty_channel_disconnect_rx_t;
 
-/*
+/**
  * The oneshot channel for closing the connection (sending end).
  *
  * On the Rust side, this is an `oneshot::Sender<CloseCode>`.
  */
 typedef struct salty_channel_disconnect_tx_t salty_channel_disconnect_tx_t;
 
-/*
+/**
  * An event channel (receiving end).
  *
  * On the Rust side, this is an `UnboundedReceiver<Event>`.
  */
 typedef struct salty_channel_event_rx_t salty_channel_event_rx_t;
 
-/*
+/**
  * An event channel (sending end).
  *
  * On the Rust side, this is an `UnboundedSender<Event>`.
  */
 typedef struct salty_channel_event_tx_t salty_channel_event_tx_t;
 
-/*
+/**
  * The channel for receiving incoming messages.
  *
  * On the Rust side, this is an `mpsc::UnboundedReceiver<MessageEvent>`.
  */
 typedef struct salty_channel_receiver_rx_t salty_channel_receiver_rx_t;
 
-/*
+/**
  * The channel for sending outgoing messages (receiving end).
  *
  * On the Rust side, this is an `mpsc::UnboundedReceiver<OutgoingMessage>`.
  */
 typedef struct salty_channel_sender_rx_t salty_channel_sender_rx_t;
 
-/*
+/**
  * The channel for sending outgoing messages (sending end).
  *
  * On the Rust side, this is an `mpsc::UnboundedSender<OutgoingMessage>`.
  */
 typedef struct salty_channel_sender_tx_t salty_channel_sender_tx_t;
 
-/*
+/**
  * A SaltyRTC client instance.
  *
  * Internally, this is a `Rc<RefCell<SaltyClient>>`.
  */
 typedef struct salty_client_t salty_client_t;
 
-/*
+/**
  * An event loop instance.
  *
  * The event loop is not thread safe.
  */
 typedef struct salty_event_loop_t salty_event_loop_t;
 
-/*
+/**
  * A handshake future. This will be passed to the `salty_client_connect`
  * function.
  *
@@ -340,19 +341,19 @@ typedef struct salty_event_loop_t salty_event_loop_t;
  */
 typedef struct salty_handshake_future_t salty_handshake_future_t;
 
-/*
+/**
  * A key pair.
  */
 typedef struct salty_keypair_t salty_keypair_t;
 
-/*
+/**
  * A remote handle to an event loop instance.
  *
  * This type is thread safe.
  */
 typedef struct salty_remote_t salty_remote_t;
 
-/*
+/**
  * The return value when encrypting or decrypting raw data.
  *
  * Note: Before accessing `bytes`, make sure to check the `success` field for
@@ -364,7 +365,7 @@ typedef struct {
   size_t bytes_len;
 } salty_client_encrypt_decrypt_ret_t;
 
-/*
+/**
  * The return value when initializing a connection.
  *
  * Note: Before accessing `connect_future`, make sure to check the `success`
@@ -377,7 +378,7 @@ typedef struct {
   const salty_channel_event_tx_t *event_tx;
 } salty_client_init_ret_t;
 
-/*
+/**
  * An event.
  *
  * If the event type is `EVENT_SERVER_HANDSHAKE_COMPLETED`, then the
@@ -394,7 +395,7 @@ typedef struct {
   uint8_t peer_id;
 } salty_event_t;
 
-/*
+/**
  * The return value when trying to receive an event.
  *
  * Note: Before accessing `event`, make sure to check the `success` field
@@ -405,7 +406,7 @@ typedef struct {
   const salty_event_t *event;
 } salty_client_recv_event_ret_t;
 
-/*
+/**
  * A message event.
  *
  * If the message type is `MSG_TASK` or `MSG_APPLICATION`, then the `msg_bytes` field
@@ -421,7 +422,7 @@ typedef struct {
   uint16_t close_code;
 } salty_msg_t;
 
-/*
+/**
  * The return value when trying to receive a message.
  *
  * Note: Before accessing `msg`, make sure to check the `success` field
@@ -432,7 +433,7 @@ typedef struct {
   const salty_msg_t *msg;
 } salty_client_recv_msg_ret_t;
 
-/*
+/**
  * The return value when creating a new client instance.
  *
  * Note: Before accessing `client` or one of the channels, make sure to check
@@ -449,42 +450,42 @@ typedef struct {
   const salty_channel_disconnect_rx_t *disconnect_rx;
 } salty_relayed_data_client_ret_t;
 
-/*
+/**
  * Free a `salty_channel_disconnect_rx_t` instance.
  */
 void salty_channel_disconnect_rx_free(const salty_channel_disconnect_rx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_disconnect_tx_t` instance.
  */
 void salty_channel_disconnect_tx_free(const salty_channel_disconnect_tx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_event_rx_t` instance.
  */
 void salty_channel_event_rx_free(const salty_channel_event_rx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_event_tx_t` instance.
  */
 void salty_channel_event_tx_free(const salty_channel_event_tx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_receiver_rx_t` instance.
  */
 void salty_channel_receiver_rx_free(const salty_channel_receiver_rx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_sender_rx_t` instance.
  */
 void salty_channel_sender_rx_free(const salty_channel_sender_rx_t *ptr);
 
-/*
+/**
  * Free a `salty_channel_sender_tx_t` instance.
  */
 void salty_channel_sender_tx_free(const salty_channel_sender_tx_t *ptr);
 
-/*
+/**
  * Connect to the specified SaltyRTC server, do the server and peer handshake
  * and run the task loop.
  *
@@ -515,7 +516,7 @@ salty_client_connect_success_t salty_client_connect(const salty_handshake_future
                                                     const salty_channel_sender_rx_t *sender_rx,
                                                     const salty_channel_disconnect_rx_t *disconnect_rx);
 
-/*
+/**
  * Decrypt raw bytes using the session keys after the handshake has been finished.
  *
  * Note: The returned data must be explicitly freed with
@@ -536,7 +537,7 @@ salty_client_encrypt_decrypt_ret_t salty_client_decrypt_with_session_keys(const 
                                                                           size_t data_len,
                                                                           const uint8_t *nonce);
 
-/*
+/**
  * Close the connection.
  *
  * Depending on whether this succeeds or not, the `disconnect_tx` instance is
@@ -547,16 +548,16 @@ salty_client_encrypt_decrypt_ret_t salty_client_decrypt_with_session_keys(const 
  * - DISCONNECT_ERROR: The `disconnect_tx` instance was freed
  *
  * Parameters:
- * disconnect_tx (`*salty_channel_disconnect_tx_t`, borrowed or moved):
- * The sending end of the channel for closing the connection.
- * This object is returned when creating a client instance.
- * close_code (`uint16_t`, copied):
- * The close code according to the SaltyRTC protocol specification.
+ *     disconnect_tx (`*salty_channel_disconnect_tx_t`, borrowed or moved):
+ *         The sending end of the channel for closing the connection.
+ *         This object is returned when creating a client instance.
+ *     close_code (`uint16_t`, copied):
+ *         The close code according to the SaltyRTC protocol specification.
  */
 salty_client_disconnect_success_t salty_client_disconnect(const salty_channel_disconnect_tx_t *disconnect_tx,
                                                           uint16_t close_code);
 
-/*
+/**
  * Free memory allocated and returned by `salty_client_encrypt_with_session_keys`
  * or `salty_client_decrypt_with_session_keys`.
  *
@@ -569,7 +570,7 @@ salty_client_disconnect_success_t salty_client_disconnect(const salty_channel_di
 void salty_client_encrypt_decrypt_free(const uint8_t *data,
                                        size_t data_len);
 
-/*
+/**
  * Encrypt raw bytes using the session keys after the handshake has been finished.
  *
  * Note: The returned data must be explicitly freed with
@@ -590,7 +591,7 @@ salty_client_encrypt_decrypt_ret_t salty_client_encrypt_with_session_keys(const 
                                                                           size_t data_len,
                                                                           const uint8_t *nonce);
 
-/*
+/**
  * Prepare a connection to the specified SaltyRTC server, but do not connect yet.
  *
  * Parameters:
@@ -619,7 +620,7 @@ salty_client_init_ret_t salty_client_init(const char *host,
                                           const uint8_t *ca_cert,
                                           uint32_t ca_cert_len);
 
-/*
+/**
  * Receive an event from the incoming channel.
  *
  * Parameters:
@@ -635,12 +636,12 @@ salty_client_init_ret_t salty_client_init(const char *host,
 salty_client_recv_event_ret_t salty_client_recv_event(const salty_channel_event_rx_t *event_rx,
                                                       const uint32_t *timeout_ms);
 
-/*
+/**
  * Free a `salty_client_recv_event_ret_t` instance.
  */
 void salty_client_recv_event_ret_free(salty_client_recv_event_ret_t recv_ret);
 
-/*
+/**
  * Receive a message from the incoming channel.
  *
  * Parameters:
@@ -656,12 +657,12 @@ void salty_client_recv_event_ret_free(salty_client_recv_event_ret_t recv_ret);
 salty_client_recv_msg_ret_t salty_client_recv_msg(const salty_channel_receiver_rx_t *receiver_rx,
                                                   const uint32_t *timeout_ms);
 
-/*
+/**
  * Free a `salty_client_recv_msg_ret_t` instance.
  */
 void salty_client_recv_msg_ret_free(salty_client_recv_msg_ret_t recv_ret);
 
-/*
+/**
  * Send an application message through the outgoing channel.
  *
  * Parameters:
@@ -676,7 +677,7 @@ salty_client_send_success_t salty_client_send_application_bytes(const salty_chan
                                                                 const uint8_t *msg,
                                                                 uint32_t msg_len);
 
-/*
+/**
  * Send a task message through the outgoing channel.
  *
  * Parameters:
@@ -691,17 +692,17 @@ salty_client_send_success_t salty_client_send_task_bytes(const salty_channel_sen
                                                          const uint8_t *msg,
                                                          uint32_t msg_len);
 
-/*
+/**
  * Free an event loop instance.
  */
 void salty_event_loop_free(const salty_event_loop_t *ptr);
 
-/*
+/**
  * Free an event loop remote handle.
  */
 void salty_event_loop_free_remote(const salty_remote_t *ptr);
 
-/*
+/**
  * Return a remote handle from an event loop instance.
  *
  * Thread safety:
@@ -715,7 +716,7 @@ void salty_event_loop_free_remote(const salty_remote_t *ptr);
  */
 const salty_remote_t *salty_event_loop_get_remote(const salty_event_loop_t *ptr);
 
-/*
+/**
  * Create a new event loop instance.
  *
  * In the background, this will instantiate a Tokio reactor core.
@@ -727,7 +728,7 @@ const salty_remote_t *salty_event_loop_get_remote(const salty_event_loop_t *ptr)
  */
 const salty_event_loop_t *salty_event_loop_new(void);
 
-/*
+/**
  * Free a `KeyPair` instance.
  *
  * Note: If you move the `salty_keypair_t` instance into a `salty_client_t` instance,
@@ -736,7 +737,7 @@ const salty_event_loop_t *salty_event_loop_new(void);
  */
 void salty_keypair_free(const salty_keypair_t *ptr);
 
-/*
+/**
  * Create a new `KeyPair` instance and return an opaque pointer to it.
  *
  * Returns:
@@ -744,7 +745,7 @@ void salty_keypair_free(const salty_keypair_t *ptr);
  */
 const salty_keypair_t *salty_keypair_new(void);
 
-/*
+/**
  * Get the private key from a `salty_keypair_t` instance.
  *
  * Returns:
@@ -755,7 +756,7 @@ const salty_keypair_t *salty_keypair_new(void);
  */
 const uint8_t *salty_keypair_private_key(const salty_keypair_t *ptr);
 
-/*
+/**
  * Get the public key from a `salty_keypair_t` instance.
  *
  * Returns:
@@ -766,7 +767,7 @@ const uint8_t *salty_keypair_private_key(const salty_keypair_t *ptr);
  */
 const uint8_t *salty_keypair_public_key(const salty_keypair_t *ptr);
 
-/*
+/**
  * Create a new `KeyPair` instance and return an opaque pointer to it.
  *
  * Parameters:
@@ -778,7 +779,7 @@ const uint8_t *salty_keypair_public_key(const salty_keypair_t *ptr);
  */
 const salty_keypair_t *salty_keypair_restore(const uint8_t *ptr);
 
-/*
+/**
  * Change the log level of the logger.
  *
  * Parameters:
@@ -791,7 +792,7 @@ const salty_keypair_t *salty_keypair_restore(const uint8_t *ptr);
  */
 bool salty_log_change_level(uint8_t level);
 
-/*
+/**
  * Initialize logging to stdout with log messages up to the specified log level.
  *
  * Parameters:
@@ -804,7 +805,7 @@ bool salty_log_change_level(uint8_t level);
  */
 bool salty_log_init(uint8_t level);
 
-/*
+/**
  * Get a pointer to the auth token bytes from a `salty_client_t` instance.
  *
  * Ownership:
@@ -812,17 +813,17 @@ bool salty_log_init(uint8_t level);
  *     Do not reuse the reference after the `salty_client_t` instance has been freed!
  * Returns:
  *     A null pointer if the parameter is null, if no auth token is set on the client
- *     or if the rc cannot be borrowed.
+ *     or if the arc cannot be borrowed.
  *     Pointer to a 32 byte `uint8_t` array otherwise.
  */
 const uint8_t *salty_relayed_data_client_auth_token(const salty_client_t *ptr);
 
-/*
+/**
  * Free a SaltyRTC client with the Relayed Data task.
  */
 void salty_relayed_data_client_free(const salty_client_t *ptr);
 
-/*
+/**
  * Initialize a new SaltyRTC client as initiator with the Relayed Data task.
  *
  * Parameters:
@@ -848,7 +849,7 @@ salty_relayed_data_client_ret_t salty_relayed_data_initiator_new(const salty_key
                                                                  const uint8_t *trusted_responder_key,
                                                                  const uint8_t *server_public_permanent_key);
 
-/*
+/**
  * Initialize a new SaltyRTC client as responder with the Relayed Data task.
  *
  * Parameters:
